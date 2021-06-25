fd3de48e209f1be6bbb0ed5e143a78e1dbdadc2a
Sarah Galed <sarajessicagaled@gmail.com>
2021-06-25 13:16:48 -0400

    unit-1-project-start

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/*-------------------------------- Constants --------------------------------*/
+
+// const colorScheme = {
+//     dark: "",
+//     change: function(){
+//         console.log(colorScheme.dark)
+//         colorScheme.dark = colorScheme.dark ? "" : "dark"
+//         document.querySelector("body").setAttribute("class", colorScheme.dark)
+//         console.log(colorScheme.dark)
+//     }
+// }
+
+/*-------------------------------- Variables --------------------------------*/
+
+const noun = [ 'abandoned home', 'airfield', 'amusement park', 'antique shop', 'apple orchard', 'arena', 'auction house', 'Bangkok, Thailand', 'bathroom', 'bazaar', 'bridge', 'Cabo', 'cafeteria', 'cemetery', 'chamber', 'church', 'construction site', 'cornfield', 'courthouse', 'crack house', 'factory reset button', 'blood rage', 'idiot', 'toaster', 'legend', 'death wish', 'therapy', 'goal in life', 'marketing idea', 'psychic', 'knife', 'sandwich', 'hunting ground', 'lettuce', 'kitty', 'friendly grandma', 'french chef', 'antidepressant drug']
+
+
+const pluralNoun
+const verb
+const adVerb
+const adjective
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
