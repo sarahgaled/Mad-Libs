d1f34e9ffbd35d720dad4fe79e65a5ae14e8a66c
Sarah Galed <sarajessicagaled@gmail.com>
2021-07-02 01:08:10 -0400

    adding a present tense array

--- js/app.js
+++ js/app.js
@@ -20,10 +20,12 @@ let selectedCategory = ""
 let nounList = ['abandoned home', 'airfield', 'amusement park', 'antique shop', 'apple orchard', 'arena', 'auction house', 'Bangkok, Thailand', 'bathroom', 'bazaar', 'bridge', 'Cabo', 'cafeteria', 'cemetery', 'chamber', 'church', 'construction site', 'cornfield', 'courthouse', 'crack house', 'factory reset button', 'blood rage', 'idiot', 'toaster', 'legend', 'death wish', 'therapy', 'goal in life', 'marketing idea', 'psychic', 'knife', 'sandwich', 'hunting ground', 'lettuce', 'kitty', 'friendly grandma', 'french chef', 'antidepressant drug']
 
 
-let adjectiveList = ['dead', 'hairless', 'sadistic', 'metal', 'wild', 'domesticated', 'abnormal', 'medicated', 'cocky', 'massive', 'disrespectful', 'impressive', 'out of control', 'internet worthy', 'hilarious', 'sexy', 'hot', 'very tactful', 'bearded', 'duck-like', 'violent', 'slimy', 'insanely creepy', 'embarrassed to the bone', 'self-centered', 'talking', 'naked', 'angry', 'shaky', 'deep', 'sick', 'zippy', 'sticky', 'fluffy', 'frozen', 'unholy', 'painfully honest', 'filthy', 'fighting', 'bonkers', 'harsh', 'frisky', 'greedy', 'crawly', 'insane', 'hideous', 'ungodly', 'abusive', 'drunken', 'hateful', 'idiotic', 'twisted', 'useless', 'yapping', 'magical', 'indecent', 'godawful', 'arrogant', 'confused', 'flirting', 'high-end', 'insecure', 'maniacal']
+let adjectiveList = ['dead', 'hairless', 'sadistic', 'metal', 'wild', 'domesticated', 'abnormal', 'medicated', 'cocky', 'massive', 'disrespectful', 'impressive', 'out of control', 'internet worthy', 'hilarious', 'sexy', 'hot', 'very tactful', 'bearded', 'duck-like', 'violent', 'slimy', 'insanely creepy', 'embarrassed to the bone', 'self-centered', 'talking', 'naked', 'angry', 'shaky', 'deep', 'sick', 'zippy', 'sticky', 'fluffy', 'frozen', 'unholy', 'painfully honest', 'filthy', 'bonkers', 'harsh', 'frisky', 'greedy', 'crawly', 'insane', 'hideous', 'ungodly', 'abusive', 'drunken', 'hateful', 'idiotic', 'twisted', 'useless', 'yapping', 'magical', 'indecent', 'godawful', 'arrogant', 'confused', 'flirting', 'high-end', 'insecure', 'maniacal']
 
 
-let verbList = ['surround', 'stab', 'return', 'medicate', 'blindside', 'boogie', 'flap', 'trip', 'swat', 'suck in', 'harass', 'trap', 'snoop', 'explode', 'sketch', 'scatter', 'challenge', 'fight', 'bury', 'splatter', 'smack', 'peddle', 'balance', 'trip up', 'boggle', 'poke', 'critique', 'fear', 'initiate', 'line up', 'run over', 'schedule', 'cook', 'imprison', 'underestimate', 'cajole', 'wheedle', 'soft soap', 'butter up', 'sweet-talk', 'prevail', 'shred', 'drink', 'dispute', 'echo', 'mimic', 'berate', 'castigate', 'underrate', 'taunt']
+let verbList = ['surround', 'stab', 'return', 'medicate', 'blindsiding', 'boogieing', 'flappig', 'tripping', 'swatting', 'sucking in', 'harassing', 'trapping', 'snooping', 'exploding', 'sketching', 'scattering', 'challenging', 'fighting', 'burying', 'splattering', 'smacking', 'peddling', 'balancing', 'tripping up', 'boggling', 'poking', 'critiquing', 'fearing', 'initiating', 'lining up', 'running over', 'scheduling', 'cook', 'imprisoning', 'underestimating', 'cajole', 'wheedling', 'soft soaping', 'buttering up', 'sweet-talk', 'prevailing', 'shredding', 'drinking', 'disputing', 'echoing', 'mimicing', 'berating', 'castigating', 'underrating', 'taunting']
+
+let presentTenseVerbList = ['surrounding', 'stabbing', 'returning', 'medicating', 'blindsiding', 'boogieing', 'flappig', 'tripping', 'swatting', 'sucking in', 'harassing', 'trapping', 'snooping', 'exploding', 'sketching', 'scattering', 'challenging', 'fighting', 'burying', 'splattering', 'smacking', 'peddling', 'balancing', 'tripping up', 'boggling', 'poking', 'critiquing', 'fearing', 'initiating', 'lining up', 'running over', 'scheduling', 'cook', 'imprisoning', 'underestimating', 'cajole', 'wheedling', 'soft soaping', 'buttering up', 'sweet-talk', 'prevailing', 'shredding', 'drinking', 'disputing', 'echoing', 'mimicing', 'berating', 'castigating', 'underrating', 'taunting']
 
 
 let adverbList = ['tightly', 'joyously', 'shyly', 'unaccountably', 'frantically', 'dreamily', 'evenly', 'enormously', 'loudly', 'joyfully', 'quaintly', 'afterwards', 'eventually', 'nervously', 'truthfully', 'fortunately', 'daily', 'poorly', 'frightfully', 'equally', 'less', 'annually', 'officially', 'acidly', 'wildly', 'dimly', 'readily', 'weakly', 'fiercely', 'quietly', 'perfectly', 'judgmentally', 'regularly', 'gently', 'madly', 'knowledgeably', 'scarily', 'helpfully', 'enormously', 'mostly', 'gladly', 'greatly', 'frightfully', 'woefully', 'irritably', 'fortunately', 'utterly', 'heavily', 'extremely', 'enthusiastically', 'reluctantly', 'patiently', 'kindly', 'faithfully', 'courageously', 'shakily', 'politely', 'daringly', 'powerfully', 'lazily', 'diligently', 'ingeniously', 'keenly', 'nearly', 'beautifully', 'cheerfully', 'physically', 'promptly', 'victoriously', 'unnecessarily', 'ferociously', 'stealthily', 'foolishly', 'valiantly', 'daintily', 'joyfully']
@@ -39,12 +41,14 @@ const reset = document.querySelector(".reset")
 const form = document.querySelector("#form")
 const noun = document.querySelector("#noun")
 const verb = document.querySelector("#verb")
+const presentVerb = document.querySelector("#present-verb")
 const adjective = document.querySelector("#adjective")
 const adverb = document.querySelector("#adverb")
 const pluralNoun = document.querySelector("#plural-noun")
 const messageElement = document.querySelector("#message")
 const randomNoun = document.querySelector("#random-noun")
 const randomVerb = document.querySelector("#random-verb")
+const randomPresentVerb = document.querySelector("#random-present-verb")
 const randomAdjective = document.querySelector("#random-adjective")
 const randomAdverb = document.querySelector("#random-adverb")
 const randomPluralNoun = document.querySelector("#random-plural-noun")
@@ -66,6 +70,7 @@ const defaultAudio = new Audio("../music/defaultAudio.mp3")
 
 randomNoun.addEventListener('click', autoFillNoun)
 randomVerb.addEventListener('click', autoFillVerb)
+randomPresentVerb.addEventListener('click', autoFillPresentVerb)
 randomAdjective.addEventListener('click', autoFillAdjective)
 randomAdverb.addEventListener('click', autoFillAdverb)
 randomPluralNoun.addEventListener('click', autoFillPluralNoun)
@@ -100,20 +105,21 @@ function generateStory(event) { //grab the input values to generate the story
     let adjective = document.getElementById('adjective').value
     let adverb = document.getElementById('adverb').value
     let pluralNoun = document.getElementById('plural-noun').value
+    let presentVerb = document.getElementById('present-verb').value
 
 
-    if (noun && verb && adjective && adverb && pluralNoun && event.target.id === 'generate-madlib') {
+    if (noun && verb && adjective && adverb && pluralNoun && presentVerb && event.target.id === 'generate-madlib') {
         madlibContent.classList.add("animate__animated", "animate__rotateIn")
         if (selectedCategory === 'dating') {
             // madlibContent.classList.add("animate__animated", "animate__rotateIn")
-            madlibContent.innerText = `I went on a date in ${noun} and it was interestingly ${adjective}. The guy couldn't stop ${verb} and ${adverb} too! Dating ${pluralNoun} is weak.`
+            madlibContent.innerText = `I went on a date in ${noun} and it was interestingly ${adjective}. The guy couldn't stop ${adverb} ${verb} too! Dating ${pluralNoun} is weak.`
             datingAudio.play()
             datingAudio.volume = .3
             setTimeout(() => datingAudio.pause(), 8000)
         }
         if (selectedCategory === 'marriage') {
             // madlibContent.classList.add("animate__animated", "animate__rotateIn")
-            madlibContent.innerText = `Marriage is like ${noun}, it's all ${noun} until you realize you made the biggest mistake by ${verb} this shmuck. Just the thought of ${adverb} makes you nauseous.`
+            madlibContent.innerText = `Marriage is like ${noun}, it's all ${noun} until you realize you made the biggest mistake by ${presentVerb} this shmuck. Just the thought of the ${adjective} ${noun} makes you nauseous.`
             marriageAudio.play()
             marriageAudio.volume = .3
             setTimeout(() => marriageAudio.pause(), 8000)
@@ -121,7 +127,7 @@ function generateStory(event) { //grab the input values to generate the story
 
         if (selectedCategory === 'adulthood') {
             // madlibContent.classList.add("animate__animated", "animate__rotateIn")
-            madlibContent.innerText = `There is nothing like a adulthood. Everyone tells you it's going to be ${noun} and ${adjective}. But then you start ${verb} and ${adverb} and you realize all ${pluralNoun}s were wrong. Adulthood sucks.`
+            madlibContent.innerText = `There is nothing like a adulthood. Everyone tells you it's going to be an ${noun} and ${adjective}. But then you start ${adverb} ${verb} and you realize all ${pluralNoun} were wrong. Adulthood sucks.`
             adulthoodAudio.play()
             adulthoodAudio.volume = .3
             setTimeout(() => adulthoodAudio.pause(), 10000)
@@ -129,7 +135,7 @@ function generateStory(event) { //grab the input values to generate the story
 
         if (selectedCategory === 'new-york') {
             // madlibContent.classList.add("animate__animated", "animate__rotateIn")
-            madlibContent.innerText = `New York is filled with ${noun}s and ${adjective}s who like to ${verb} and ${adverb} all over the place with ${pluralNoun}`
+            madlibContent.innerText = `New York is filled with ${noun}s and ${pluralNoun}s who like to ${verb} ${adverb} all over the place with ${pluralNoun}`
             newYorkAudio.play()
             newYorkAudio.volume = .3
             setTimeout(() => newYorkAudio.pause(), 8000)
@@ -151,6 +157,7 @@ function resetStory() { //reseting all the inputs to strings which essentially r
     adjective.value = ""
     adverb.value = ""
     pluralNoun.value = ""
+    presentVerb.value = ""
 
     madlibContent.innerHTML = ""
 
@@ -181,6 +188,10 @@ function autoFillVerb() {
     verb.value = generateRandom(verbList)
 }
 
+function autoFillPresentVerb() {
+    presentVerb.value = generateRandom(presentTenseVerbList)
+}
+
 function autoFillAdjective() {
     adjective.value = generateRandom(adjectiveList)
 }
@@ -199,6 +210,7 @@ function totalAutoFill() {
     adjective.value = generateRandom(adjectiveList)
     adverb.value = generateRandom(adverbList)
     pluralNoun.value = generateRandom(pluralNounList)
+    presentVerb.value = generateRandom(presentTenseVerbList)
 }
 
 
